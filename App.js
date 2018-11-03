6c9c5593fde6f9d50847ca624ab68839d1dc7538
Maddie Kasula <madelynkasula@gmail.com>
2018-11-02 22:45:03 -0700

    Basic Apollo setup

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import { ApolloClient} from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
+import { ApolloProvider } from 'react-apollo';
+import { withClientState } from 'apollo-link-state';
 import { createStackNavigator } from 'react-navigation';
 import NavigationScreen from './screens/NavigationScreen';
 import HomeScreen from './screens/HomeScreen';
@@ -6,6 +12,28 @@ import ScheduleScreen from './screens/ScheduleScreen';
 import FavesScreen from './screens/FavesScreen';
 import MessagesScreen from './screens/MessagesScreen';
 import SettingsScreen from './screens/SettingsScreen';
+
+const cache = new InMemoryCache()
+const stateLink = withClientState({
+  cache,
+  defaults: {
+    testing: {
+      __typename: 'testing',
+      name: '',
+      age: 0
+    }
+  }
+})
+const client = new ApolloClient({
+  cache,
+  link: ApolloLink.from([
+    stateLink,
+    new HttpLink({ uri: 'http://localhost:3000/graphql' })
+  ])
+})
+
+// make client to rewrite the defaults every time the store resets
+client.onResetStore(stateLink.writeDefaults)
 
 const RootStack = createStackNavigator(
   {
@@ -23,6 +51,10 @@ const RootStack = createStackNavigator(
 
 export default class App extends React.Component {
   render() {
-    return <RootStack />;
+    return (
+      <ApolloProvider client={client}>
+        <RootStack />
+      </ApolloProvider>
+    );
   }
 }
