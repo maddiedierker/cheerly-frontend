d2dbc023e77554f8c122aa9d6c8637a6a767491b
Maddie Kasula <madelynkasula@gmail.com>
2018-11-11 17:36:22 -0800

    Move apolloClient into its own file

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import { AsyncStorage } from 'react-native'
-import { ApolloClient} from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { createHttpLink } from 'apollo-link-http'
-import { setContext } from 'apollo-link-context'
+import { client } from './apolloClient'
 import { ApolloProvider } from 'react-apollo'
 import { createStackNavigator } from 'react-navigation'
 import NavigationScreen from './screens/NavigationScreen'
@@ -13,33 +9,6 @@ import FavesScreen from './screens/FavesScreen'
 import MessagesScreen from './screens/MessagesScreen'
 import SettingsScreen from './screens/SettingsScreen'
 import SignInScreen from './screens/SignInScreen'
-
-const httpLink = new createHttpLink({ uri: 'http://localhost:3000/graphql' })
-let cachedToken
-const authLink = setContext((_, { headers }) => {
-  if (cachedToken) {
-    return {
-      headers: {
-        authorization: `Bearer ${token}`
-      }
-    }
-  }
-
-  return AsyncStorage.getItem('token').then(token => {
-    cachedToken = token
-    return {
-      headers: {
-        authorization: `Bearer ${token}`
-      }
-    }
-  })
-})
-
-const cache = new InMemoryCache()
-const client = new ApolloClient({ 
-  link: authLink.concat(httpLink), 
-  cache 
-})
 
 const RootStack = createStackNavigator(
   {
---
+++ apolloClient.js
@@ -0,0 +1,32 @@
+import { AsyncStorage } from 'react-native'
+import { ApolloClient} from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+
+// TODO: get GraphQL URI from env variable
+const httpLink = new createHttpLink({ uri: 'http://localhost:3000/graphql' })
+const cache = new InMemoryCache()
+let cachedToken
+
+const getToken = () => {
+  if (cachedToken) {
+    return cachedToken
+  } else {
+    return AsyncStorage.getItem('token').then(token => {
+      cachedToken = token
+      return cachedToken
+    })
+  }
+}
+
+const authLink = setContext((_, { headers }) => {
+  headers: {
+    authorization: `Bearer ${getToken()}`
+  }
+})
+
+export const client = new ApolloClient({ 
+  link: authLink.concat(httpLink), 
+  cache 
+})
